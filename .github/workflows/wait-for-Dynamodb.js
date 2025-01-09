35d314a4e2276feeb35595b5b092ca82f68507c7
Kenneth Fernandes <57480946+Kenneth-Fernandes-0620@users.noreply.github.com>
2025-01-09 21:56:05 +0530

    Added code to wait for dynamoDB to startup in Github Actions

---
+++ .github/workflows/wait-for-Dynamodb.js
@@ -0,0 +1,34 @@
+// wait-for-dynamodb.js
+import { setTimeout } from 'timers/promises'
+import { DynamoDBClient, ListTablesCommand } from '@aws-sdk/client-dynamodb'
+
+const dynamodbClient = new DynamoDBClient({ region: 'ap-south-1' })
+
+let iteration = 500;
+
+const waitForDynamoDbToStart = async () => {
+    iteration = iteration - 1;
+
+    if (iteration == 0){
+        throw new Error("Failed to start DynamoDB");
+    }
+    try {
+        const command = new ListTablesCommand({})
+        await dynamodbClient.send(command)
+    } catch (error) {
+        console.log('Waiting for Docker container to start...')
+        await setTimeout(500)
+        return waitForDynamoDbToStart()
+    }
+}
+
+const start = Date.now()
+waitForDynamoDbToStart()
+    .then(() => {
+        console.log(`DynamoDB-local started after ${Date.now() - start}ms.`)
+        process.exit(0)
+    })
+    .catch(error => {
+        console.log('Error starting DynamoDB-local!', error)
+        process.exit(1)
+    })
